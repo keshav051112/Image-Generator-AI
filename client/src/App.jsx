19924713553933d716ba41dd6ad6cd5970be23eb
keshav051112 <160417729+keshav051112@users.noreply.github.com>
2025-04-11 22:58:39 +0530

    frontend part completed

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Home from './pages/Home'
+import  Result  from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-oragne-50'>
+      <Navbar/>
+    <Routes>
+      <Route path='/'  element={<Home/>} />
+      <Route path='/result'  element={<Result/>} />
+      <Route path='/buy'  element={<BuyCredit/>} />
+    </Routes>
+      
+    </div>
+  )
+}
+
+export default App
