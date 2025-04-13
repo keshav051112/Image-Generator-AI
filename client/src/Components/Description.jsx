0fc7800c5696baa6f7d68889f8be885cabd95a9f
keshav051112 <160417729+keshav051112@users.noreply.github.com>
2025-04-13 23:41:24 +0530

    day3

--- client/src/Components/Description.jsx
+++ client/src/Components/Description.jsx
@@ -14,6 +14,7 @@ const Description = () => {
                 <h2 className='text-3xl font-medium max-w-lg mb-4'>Introducing the AI-Powered Text to Image Generator</h2>
                 <p className='text-gray-600 mb-4'>Easily bring your ideas to life with our free AI image generator. Whether you need stunning visuals or unique imagery, our tool transforms your text into eye-catching images with just a few clicks. Imagine it, describe it, and watch it come to life instantly.</p>
                 <p className='text-gray-600 '>Simply type in a text prompt, and our cutting-edge AI will generate high-quality images in seconds. From product visuals to character designs and portraits, even concepts that don’t yet exist can be visualized effortlessly. Powered by advanced AI technology, the creative possibilities are limitless!</p>
+                <p className='text-gray-600 '>Simply type in a text prompt, and our cutting-edge AI will generate high-quality images in seconds. From product visuals to character designs and portraits, even concepts that don’t yet exist can be visualized effortlessly. Powered by advanced AI technology, the creative possibilities are limitless!</p>
             </div>
          </div>
     </div>
